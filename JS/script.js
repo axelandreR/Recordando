d935e531d41c7eb90abeee3baf3daf116a07e4a0
Axel Rodríguez Criollo <47278265+axelandreR@users.noreply.github.com>
2022-02-14 07:03:35 -0500

    form2

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,39 @@
-// let edad = parseInt(prompt("¿Cuál es tu edad?", 13));
+'use strict'
 
-// edad >= 18? alert("Mayor de edad"): alert("Menor de edad");
+window.addEventListener('load', function(){
+    let formulario = document.querySelector('#form');
+
+    let baseConversion = document.getElementsByClassName('conversion');
+    
+    function convertir(cantidad, UnidadMedida){
+        let base = UnidadMedida=="Docena"? cantidad:false
+    }
+    formulario.addEventListener('submit', function(){
+        console.log(baseConversion);
+    });
+});
+
+
+let num;
+do {
+num = prompt("Ingresa un número mayor a 100", 0);
+} while (num <= 100 && num);
+// let inicio =prompt("Desea iniciar sesión")
+// if(inicio == "Si"){
+//     let usuario = prompt("Ingrese su usuario:");
+//     if(usuario == "Admin"){
+//         let contraseña = prompt("Ingrese Contraseña:");
+//         if(contraseña== "TheMaster"){
+//             alert("Bienvenido");
+//         }else if(contraseña == " "|| contraseña == null){
+//             alert("Cancelado");
+//         }else{
+//             alert("Contraseña incorrecta");
+//         }
+//     }else{
+//         alert("No te conozco")
+//     }
+
+// }else{
+//     alert("Hasta pronto")
+// }
